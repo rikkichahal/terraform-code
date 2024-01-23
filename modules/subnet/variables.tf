f737c03cb4bca1f767e2aee28a1bec2ff4578344
rikkichahal <chahalrikki@gmail.com>
2024-01-23 17:51:52 +0530

    update rk

--- modules/subnet/variables.tf
+++ modules/subnet/variables.tf
@@ -1,15 +1,15 @@
 variable "subnet_name" {
-  
+    type = string
 }
 
 variable "rgname" {
-  
+    type = string
 }
 
 variable "address_prefixes" {
-  
+    type = string
 }
 
 variable "vnet_name" {
-  
+    type = string
 }
