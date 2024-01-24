1f823420ce53476740bb02281123365dabdb4b03
rikkichahal <chahalrikki@gmail.com>
2024-01-24 18:34:03 +0530

    code complete by rk

--- modules/nsg/main.tf
+++ modules/nsg/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_network_security_group" "example" {
-  count = 3
+  
   name                = var.nsg_name
   location            = var.location
   resource_group_name = var.rgname
